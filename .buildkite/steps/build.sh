38a6c0c00f5c27487a57e46748c61a3771ef9743
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-09-04 12:57:39 +0200

    added buildkite support

---
+++ .buildkite/steps/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+set -x
+
+BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
+
+echo "--- Building $PROJECT..."
+bundle exec jekyll build
+tar czf site.tar.gz _site/
+buildkite-agent artifact upload site.tar.gz
