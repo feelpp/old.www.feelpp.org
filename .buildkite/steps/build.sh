023d884eba559bdb3290dc03aabc63543f0ab1ea
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2017-09-04 15:34:14 +0200

    install missing gems just in case

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -6,6 +6,7 @@ set -x
 BRANCH=${BRANCH:-${BUILDKITE_BRANCH:master}}
 
 echo "--- Building $PROJECT..."
+bundle install
 bundle exec jekyll build
 tar czf site.tar.gz _site/
 buildkite-agent artifact upload site.tar.gz
